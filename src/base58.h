db70e3045adc9e651705cec5e5f817b9dc9b16fb
Procoin-project <jonathanm800@gmail.com>
2014-02-18 22:32:58 -0500

    Update base58.h

--- src/base58.h
+++ src/base58.h
@@ -272,7 +272,7 @@ class CBitcoinAddress : public CBase58Data
 public:
     enum
     {
-        PUBKEY_ADDRESS = 48, // Procoin addresses start with L
+        PUBKEY_ADDRESS = 22, // Procoin addresses start with P
         SCRIPT_ADDRESS = 5,
         PUBKEY_ADDRESS_TEST = 111,
         SCRIPT_ADDRESS_TEST = 196,
